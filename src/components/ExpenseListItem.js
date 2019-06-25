2b57fb8fa4932489a73e559757a41767a310c0e4
unknown <minghaozhou123@gmai.com>
2019-06-25 17:24:08 -0400

    numeral js

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({dispatch, id , description, amount, createdAt}) => (
   <div>
     <Link to={`/edit/${id}`}>   <h3>{description}</h3></Link>
-    <p>{amount}</p>
-    <p>{createdAt}</p>
-    <p>{id}</p>
+    <p>{numeral(amount / 100).format('$0,0.00')}</p>
+    <p>{moment(createdAt).format('MMMM Do YYYY')}</p>
   </div>
 );
 
